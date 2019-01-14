d6f368b997f35dbc19e6cf0f8f2865eb517592a1
Bastien Tran <bastien.tran@gmail.com>
2019-01-14 01:52:06 +0100

    factorized

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,48 +1,111 @@
 // Names and Input
-var hacker1 = prompt("Enter the driver's name"); console.log("The driver's name is " + hacker1);
+var hacker1;
+var hacker2;
+
+function getNameFor(userSlot, role) {
+  window[userSlot] = prompt("Enter the " + role + "'s name:");
+  console.log("The " + role + "'s name is " + window[userSlot]);
+};
+
+getNameFor("hacker1", "driver");
+getNameFor("hacker2", "navigator");
+
+// Conditionals
+function longestString(name1, name2, role1, role2) {
+  if (name1.length === name2.length) {
+    console.log("wow, you both got equally long names, " + name1.length + " characters!!");
+  } else if (name1.length <= name2.length) {
+    console.log("Yo, " + role2 + " got the longest name, it has " + name2.length + " characters"); 
+  } else {
+    console.log("The " + role1 + " has the longest name, it has " + name1.length + " characters"); 
+  }  
+};
+
+longestString(hacker1, hacker2, "driver", "navigator");
 
 
-var hacker2 = prompt("Enter the navigator's name"); console.log("The navigator's name is " + hacker2);
- if (hacker1.length === hacker2.length) {
-   console.log("wow, you both got equally long names, " + hacker1.length + " characters!!") }
- else if (hacker1.length <= hacker2.length) {
-   console.log("Yo, navigator got the longest name, it has " + hacker2.length + " characters"); }
- else {
-   console.log("The Driver has the longest name, it has " + hacker1.length + " characters"); }
+// Loops
+function logToUpperCaseWithSpaces (inputString) {
+  var stringToUpperCaseWithSpaces = "";
+  for (i = 0; i <= inputString.length -1; i++) {
+    stringToUpperCaseWithSpaces += (inputString[i].toUpperCase())+" ";
+  }
+console.log(stringToUpperCaseWithSpaces);
+};
+
+logToUpperCaseWithSpaces(hacker1);
+
+function logReverseString (inputString) {
+  var reversedString = "";
+  for (i = inputString.length -1; i > -1 ; i--) {
+  reversedString += (inputString[i]);
+  }
+console.log(reversedString);
+return reversedString;
+};
+
+logReverseString(hacker2);
+
+function firstInLexicalOrder (inputString1, inputString2) {
+  switch(inputString1.localeCompare(inputString2)) {
+    case 0:
+      console.log("What?! You both got the same name?");
+      return true;
+      break;
+    case 1:
+      console.log("The driver's name goes first");
+      break;
+    case -1:
+      console.log("Yo, the navigator goes first definitely");
+      break;  
+  } 
+};
+
+firstInLexicalOrder(hacker1, hacker2);
+
+function normalizeText (inputText) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz ';
+   i = 0
+  while (i < inputText.length) {
+    if (alphabet.indexOf(inputText[i].toLowerCase()) === -1) {
+      inputText = inputText.slice(0,i)+inputText.slice(i+1);
+    } else {
+      i++;
+    };
+  };
+  return inputText; 
+};
 
 
-var spaceDriver = "";
-for (i = 0; i <= hacker1.length -1; i++) {
- spaceDriver+=(hacker1[i])+" ";
+function isPalindrome () {
+  var testedString = prompt("Entrez la séquence à tester:");
+  testedString = normalizeText(testedString.toLowerCase());
+  if (testedString.localeCompare(logReverseString(testedString)) === 0) {
+    console.log("That's a nice one, and I've seen a few palindroms in my day");
+  } else {
+    console.log("Sorry, not this time... Spoonerism maybe?");
+  }
+  return true;
 }
-console.log(spaceDriver.toUpperCase());
+
+isPalindrome();
 
 
-var reverseNavigator = "";
-for (i = hacker2.length -1; i >=0; i--) {
- reverseNavigator+=(hacker2[i]);
-}
-console.log(reverseNavigator);
+function textMiner () {
+  var minedString = prompt("Enter a text to process:");
+  var searchedTerm = prompt("Which term do you wish to count:");
+  minedString = normalizeText(minedString);
+  var termArray = minedString.split(" ");
+  var termCount = 0;
+  console.log("Your text contains " + termArray.length + " terms.");
+  for (i = 0 ; i < termArray.length; i++) {
+    if (termArray[i] === searchedTerm) {
+      termCount++;
+    };
+  };
+  console.log("The term '" + searchedTerm + "' occurs " + termCount + " times in your text!");
+  console.log("'"+termArray[termArray.length-1]+"'");
+};
 
-
-
-
-//Conditionals
-
-if (hacker1 === hacker2) {
-    console.log("What?! You both got the same name?");
-   } else if (hacker1 < hacker2) {
-    console.log("The driver's name goes first");
-   } else {
-    console.log("Yo, the navigator goes first definitely");
-   };
-   
-   var alphabet = "abcdefghijklmnopqrstuvwxyz";
-   var candidate = prompt("Testez la palindromie du mot");
-   for (i=0 ; i <= candidate.length-1; i++) {
-    if (!alphabet.includes(candidate[i])) {candidate=candidate.substr(0,i-1)+candidate.substr(i+1,candidate.length-1);
-    i--;}
-   }
-   console.log(candidate);
-
-// Lorem ipsum generator
+textMiner();
+};
